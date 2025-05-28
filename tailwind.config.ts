ec82354f92b7b3e9f4e3c6d0046f12a8fd6a6fb3
Hyein <snhyein2001@gmail.com>
2025-05-28 19:03:48 +0900

    [chore] 05/28 초기 개발 환경 설정 및 Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
+
