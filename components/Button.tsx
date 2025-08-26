a49c29cab9fae7ffc6015ab9dd0e38ed5f9135a1
Hyein <snhyein2001@gmail.com>
2025-08-26 23:08:23 +0900

    chore: 프로젝트 초기세팅

---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/native';
+
+export const Button = styled.TouchableOpacity`
+  background-color: #FF7710;
+  padding: 12px 16px;
+  border-radius: 12px;
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
