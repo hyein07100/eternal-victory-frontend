4fa57ac33b5cfab299da2dd939c5666b0e649d42
Hyein <snhyein2001@gmail.com>
2025-08-30 23:44:10 +0900

    feat: 구글 버튼 구글 로그인 프론트 연동

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import * as S from "./styles";
+import React, { useEffect } from "react";
 import { Dimensions } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { makeRedirectUri } from "expo-auth-session";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import * as S from "./styles";
 import GoogleButtonSVG from "@assets/login/google.svg";
 import Ball from "@assets/login/ball.svg";
 import Glove from "@assets/login/glove.svg";
@@ -10,7 +14,15 @@ import { PageContainer } from "@components/PageContainer";
 
 const { width, height } = Dimensions.get("window");
 
+WebBrowser.maybeCompleteAuthSession();
+
 export default function LoginScreen() {
+  const redirectUri = process.env.EXPO_PUBLIC_REDIRECT_URI;
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+    redirectUri,
+  });
+
   return (
     <PageContainer>
       <S.Slogan>기적의 순간을, {"\n"}기록으로 남기다.</S.Slogan>
@@ -28,7 +40,12 @@ export default function LoginScreen() {
       </S.GloveWrapper>
 
       <S.BottomRight>
-        <S.GooglePressable onPress={() => console.log("Google Login Pressed")}>
+        <S.GooglePressable
+          onPress={() => {
+            if (request) promptAsync(); 
+          }}
+          disabled={!request}
+        >
           <GoogleButtonSVG width={width * 0.51} height={height * 0.06} />
         </S.GooglePressable>
       </S.BottomRight>
